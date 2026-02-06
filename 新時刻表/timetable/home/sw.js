86f44f355d838ae31837ef1e313179b72565a606
funwithme2021 <125599198+funwithme2021@users.noreply.github.com>
2026-02-07 03:24:45 +0800

    Add files via upload

--- 新時刻表/timetable/home/sw.js
+++ 新時刻表/timetable/home/sw.js
@@ -1,13 +1,13 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 
 self.addEventListener('notificationclick', event => {
   event.notification.close();
   const url = event.notification.data?.url || './index.html';
   event.waitUntil(
-    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
-      for (const c of clientList) {
-        if ('focus' in c) return c.focus();
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clients => {
+      for (const client of clients) {
+        if ('focus' in client) return client.focus();
       }
       if (self.clients.openWindow) return self.clients.openWindow(url);
     })
